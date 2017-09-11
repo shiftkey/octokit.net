c0713c2398c0a792d0a080bcd99164b4f275f08d
Brendan Forster <brendan@github.com>
2017-09-11 19:13:24 +1000

    wrapping HttpClient for testability

---
+++ Octokit.Tests.Integration/HttpClientWrapper.cs
@@ -0,0 +1,32 @@
+﻿using Octokit.Internal;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Octokit.Tests.Integration
+{
+    public class HttpClientWrapper : IHttpClient
+    {
+        private readonly HttpClient inner;
+
+        public HttpClientWrapper(HttpClient inner)
+        {
+            this.inner = inner;
+        }
+
+        public void Dispose()
+        {
+            inner.Dispose();
+        }
+
+        public async Task<IResponse> Send(IRequest request, CancellationToken cancellationToken)
+        {
+            var fullUrl = new Uri(request.BaseAddress, request.Endpoint);
+            var innerRequest = request.BuildMessage();
+            var innerResponse = await inner.SendAsync(innerRequest);
+            var response = await innerResponse.BuildResponse();
+            return response;
+        }
+    }
+}
